f2c34c2b45941aade3ef51094009b9b15b60f120
Frank Thamel <w.frankthamel@gmail.com>
2019-11-02 12:18:06 +0530

    Initial Commit

---
+++ ZaloraAssignment/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ZaloraAssignment
+//
+//  Created by Frank Emmanuel on 11/2/19.
+//  Copyright © 2019 Frank Thamel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
